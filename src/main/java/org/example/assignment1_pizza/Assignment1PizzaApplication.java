6881151a8f6c7cb5cb3ae23383c7a413314eec53
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-02-08 16:50:55 -0500

    Initial commit

---
+++ src/main/java/org/example/assignment1_pizza/Assignment1PizzaApplication.java
@@ -0,0 +1,13 @@
+package org.example.assignment1_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1PizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1PizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assignment1_pizza/Assignment1PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment1_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1PizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
