470837b775ad7ad74e457eb8473d50585a48f386
Abdllah Abbara <181393678+Qxx05@users.noreply.github.com>
2026-02-08 17:06:50 -0500

    Added PizzaOrder model, Completed model class, Added getters and setters

---
+++ src/main/java/org/example/assignment1_pizza/model/PizzaOrder.java
@@ -0,0 +1,117 @@
+package org.example.assignment1_pizza.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaOrder {
+
+    private int id;
+    private String customerName;
+    private String size;
+    private String crust;
+    private List<String> toppings = new ArrayList<>();
+    private int quantity;
+    private boolean delivery;
+    private String deliveryAddress;
+
+    private double subtotal;
+    private double discountAmount;
+    private double tax;
+    private double total;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCrust() {
+        return crust;
+    }
+
+    public void setCrust(String crust) {
+        this.crust = crust;
+    }
+
+    public List<String> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<String> toppings) {
+        this.toppings = toppings;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(boolean delivery) {
+        this.delivery = delivery;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
